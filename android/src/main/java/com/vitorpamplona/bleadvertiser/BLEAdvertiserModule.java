3c16c7cdf792014d1ab5b8061dacda1a877b7c14
Builty <greg.s@zoneempire.com>
2024-06-13 11:46:30 +1000

    Update BLEAdvertiserModule.java

--- android/src/main/java/com/vitorpamplona/bleadvertiser/BLEAdvertiserModule.java
+++ android/src/main/java/com/vitorpamplona/bleadvertiser/BLEAdvertiserModule.java
@@ -467,7 +467,7 @@ public class BLEAdvertiserModule extends ReactContextBaseJavaModule {
             dataBuilder.setIncludeTxPowerLevel(options.getBoolean("includeTxPowerLevel"));
         
         dataBuilder.addManufacturerData(companyId, payload);
-        dataBuilder.addServiceUuid(uuid);
+        // builty mod  dataBuilder.addServiceUuid(uuid);
         return dataBuilder.build();
     }
 
